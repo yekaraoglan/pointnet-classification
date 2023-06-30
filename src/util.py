ab7de0887a42ceaa973acf2b8b31e92c18f4eaa6
Yunus Emre Karaoğlan <karaoglan18@itu.edu.tr>
2023-06-30 11:59:55 +0300

    Borrowed util functions to visualize

---
+++ src/util.py
@@ -0,0 +1,105 @@
+# Contains utility functions 
+# source: https://www.kaggle.com/code/balraj98/pointnet-for-3d-object-classification-pytorch/notebook
+
+import numpy as np
+import plotly.graph_objects as go
+import random
+random.seed(42)
+
+def read_off_file(file):
+    # Reads the OFF file and returns the vertices and faces
+    if 'OFF' != file.readline().strip():
+        raise('Not a valid OFF header')
+    n_verts, n_faces, _ = tuple([int(s) for s in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return verts, faces
+
+def visualize_rotate(data):
+    x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
+    frames=[]
+
+    def rotate_z(x, y, z, theta):
+        w = x+1j*y
+        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
+
+    for t in np.arange(0, 10.26, 0.1):
+        xe, ye, ze = rotate_z(x_eye, y_eye, z_eye, -t)
+        frames.append(dict(layout=dict(scene=dict(camera=dict(eye=dict(x=xe, y=ye, z=ze))))))
+    fig = go.Figure(data=data,
+        layout=go.Layout(
+            updatemenus=[dict(type='buttons',
+                showactive=False,
+                y=1,
+                x=0.8,
+                xanchor='left',
+                yanchor='bottom',
+                pad=dict(t=45, r=10),
+                buttons=[dict(label='Play',
+                    method='animate',
+                    args=[None, dict(frame=dict(duration=50, redraw=True),
+                        transition=dict(duration=0),
+                        fromcurrent=True,
+                        mode='immediate'
+                        )]
+                    )
+                ])]
+        ),
+        frames=frames
+    )
+
+    return fig
+
+
+def pcshow(xs,ys,zs):
+    data=[go.Scatter3d(x=xs, y=ys, z=zs,
+                                   mode='markers')]
+    fig = visualize_rotate(data)
+    fig.update_traces(marker=dict(size=2,
+                      line=dict(width=2,
+                      color='DarkSlateGrey')),
+                      selector=dict(mode='markers'))
+    fig.show()
+
+class MeshToPointCloud(object):
+    def __init__(self, output_size):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+
+    def triangle_area(self, pt1, pt2, pt3):
+        side_a = np.linalg.norm(pt1 - pt2)
+        side_b = np.linalg.norm(pt2 - pt3)
+        side_c = np.linalg.norm(pt3 - pt1)
+        s = 0.5 * ( side_a + side_b + side_c)
+        return max(s * (s - side_a) * (s - side_b) * (s - side_c), 0)**0.5
+
+    def sample_point(self, pt1, pt2, pt3):
+        # barycentric coordinates on a triangle
+        # https://mathworld.wolfram.com/BarycentricCoordinates.html
+        s, t = sorted([random.random(), random.random()])
+        f = lambda i: s * pt1[i] + (t-s)*pt2[i] + (1-t)*pt3[i]
+        return (f(0), f(1), f(2))
+        
+    def __call__(self, mesh):
+        verts, faces = mesh
+        verts = np.array(verts)
+        areas = np.zeros((len(faces)))
+
+        for i in range(len(areas)):
+            areas[i] = (self.triangle_area(verts[faces[i][0]],
+                                           verts[faces[i][1]],
+                                           verts[faces[i][2]]))
+            
+        sampled_faces = (random.choices(faces, 
+                                      weights=areas,
+                                      cum_weights=None,
+                                      k=self.output_size))
+        
+        sampled_points = np.zeros((self.output_size, 3))
+
+        for i in range(len(sampled_faces)):
+            sampled_points[i] = (self.sample_point(verts[sampled_faces[i][0]],
+                                                   verts[sampled_faces[i][1]],
+                                                   verts[sampled_faces[i][2]]))
+        
+        return sampled_points
